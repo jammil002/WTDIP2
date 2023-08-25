68117b8ed35c992ae81f628d85121a0ec8ebc166
James Miller <jam1211miller@gmail.com>
2023-08-25 05:26:15 -0700

    WTDIPHX2

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      "bebas-neue": ["Bebas Neue", "cursive"],
+      oswald: ["Oswald", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
